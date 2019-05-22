6142bffa1f02867b5509877238c9a75d87b811df
cuining <cuining@asiainfo-sec.com>
2019-05-22 12:05:33 +0800

    静态代理

---
+++ src/main/java/cn/njyazheng/proxy/staticproxy/UserDaoProxy.java
@@ -0,0 +1,23 @@
+package cn.njyazheng.proxy.staticproxy;
+
+/**
+ * 静态代理在使用时,需要定义接口或者父类,
+ * 被代理对象与代理对象一起实现相同的接口或者是继承相同父类.
+ */
+public class UserDaoProxy implements UserDao {
+    private UserDao userDao;
+    public UserDaoProxy(){}
+    public UserDaoProxy(UserDao userDao) {
+        this.userDao = userDao;
+    }
+    
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+    
+    public void save() {
+        System.out.println("--------------------开启事务");
+        userDao.save();
+        System.out.println("--------------------提交事务");
+    }
+}
